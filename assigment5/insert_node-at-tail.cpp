e617bf9be71e6994a1ab3260c765ce583af61b35
mayanksayanengg <107642082+mayanksayanengg@users.noreply.github.com>
2022-10-18 00:22:55 +0530

    Create insert_node-at-tail.cpp

---
+++ assigment5/insert_node-at-tail.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+        }
+
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+    SinglyLinkedListNode* n =  (SinglyLinkedListNode*)malloc(sizeof(SinglyLinkedListNode));
+    n->data=data;
+    n->next=NULL;
+    
+    if(head==NULL){
+        return n;
+        
+    }
+    
+    SinglyLinkedListNode* last =head;
+    while(last->next!=NULL){
+        last=last->next;
+    }
+    last->next=n;
+    return head;
+    
+
+
+}
+
+
+
